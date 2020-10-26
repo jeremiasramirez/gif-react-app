14c9645c58433ab00098868a965e45e02513d2ed
Jeremias ramirez <jereramirez499@gmail.com>
2020-10-25 21:25:54 -0400

    haciendo pagina de busqueda

--- src/pages/search/search.page.js
+++ src/pages/search/search.page.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Menu from '../../components/menu/menu.component'
+import "./search.css"
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField';
 
 const SearchPage = () =>{
 
     return <>
             <Menu />
+
+            <section className="containersearch animate">
+                 <TextField id="standard-basic" variant="outlined" label="Search" className="fieldSearch" />
+                <article>
+                    <Button className="buttonSearch" color="primary">Ok</Button>
+                </article>
+            </section>
         </>
 
 
