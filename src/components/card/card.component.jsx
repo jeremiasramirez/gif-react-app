89e343bfa743c6c0ff3afec774cf4a18f06ab865
Jeremias ramirez <jereramirez499@gmail.com>
2020-11-01 16:17:10 -0400

    cambiado de formato a jsx + creando clase generadora de backdrop de gif

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from "@material-ui/core/Card"
+import CardHeader from "@material-ui/core/CardHeader"
+import ScreenGif from '../screengif/screen.component'
+
+
+const handleScreen = (title,img)=>{
+    ScreenGif(title,img)
+ }
+
+const CardGif = ({data})=>{
+
+   return data.map((gif,key)=>{
+       
+        return gif.images.downsized.url ? <Card className="cardGif animate" key={key} onClick={()=>handleScreen(gif.title,gif.images.downsized.url)}>
+           
+            <CardHeader title={gif.title.substring(0,14) + '..'} ></CardHeader>
+
+            <img className='animate'  src={gif.images.downsized.url} alt={gif.title} ></img>
+
+            </Card> : null
+    }) 
+
+
+
+}
+
+export default CardGif
